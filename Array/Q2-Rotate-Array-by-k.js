484dba284ac10f2be44cc9b86043e5708614b620
Ritikraj3 <ritikraj5776@gmail.com>
2025-06-19 16:15:24 +0530

    Rotate array

---
+++ Array/Q2-Rotate-Array-by-k.js
@@ -0,0 +1,24 @@
+// Ques 2 - rotate array by k
+// Given an array, rotate the array by k steps, where k is non-negative.
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3 -------->>>>>> Output: [5,6,7,1,2,3,4]
+
+// Input: nums = [-1,-100,3,99], k = 2 -------->>>>>> Output: [3,99,-1,-100]
+
+// discuss => https://leetcode.com/problems/rotate-array/solutions/
+
+//* 1st Approach
+
+ function rotation (nums, k) {
+    let size = nums.length;
+
+    if (k > size) k = k % size;
+
+    const rotated = nums.splice(size - k, k);
+
+    nums.unshift(...rotated); 
+
+    console.log(nums)
+ }
+
+rotation([1,2,3,4,5,6,7], 3)
